28a2a4d1117e2f96d7477e63cbf7ea69236f344b
Wagner <wagnertri1980@gmail.com>
2023-04-12 10:05:25 -0300

    Conectando o Jsoup no Google

--- src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
@@ -1,13 +1,42 @@
 package br.com.phc.brasileiraoapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import br.com.phc.brasileiraoapi.util.dto.PartidaGoogleDTO;
+
 public class ScrapingUtil {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
 	private static final String BASE_URL_GOOGLE = "https://www.google.com/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
+		
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformacoesPartida(url);
 	}
-
+	public PartidaGoogleDTO obtemInformacoesPartida(String url) {
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+		
+		Document document = null;
+		
+		try {
+			document = Jsoup.connect(url).get();
+			
+			//Recuperar o título da página pesquisada.
+			String title = document.title();
+			LOGGER.info("Titulo da pagina: {}", title);
+				
+		} catch(IOException e) {
+			LOGGER.error("Erro ao tentar conectar no Google com Jsoup -> {}", e.getMessage());
+		}
+		
+		return partida;
+	}	
 }
